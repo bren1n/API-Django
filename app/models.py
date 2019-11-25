76b5a8bd0b43f4004a58c8f7c850f07514aa6b40
Breno <breno.leite@parnamirim.rn.gov.br>
2019-11-25 16:45:24 -0300

    Creating Note model and serializer

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django import utils
 
 # Create your models here.
+class Note(models.Model):
+    class Meta:
+        db_table = 'note'
+
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    createdAt = models.DateTimeField(default=utils.timezone.now)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title

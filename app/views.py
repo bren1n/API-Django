42da2aeb31719840a0fa5ee3f723d6287f2bd0c8
Breno Ferreira <brenoleite1234@gmail.com>
2019-11-25 20:18:20 -0300

    Creating API methods and updating gitignore

--- app/views.py
+++ app/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Note
+from .serializers import *
+from rest_framework import permissions
 
-# Create your views here.
+
+@api_view(['GET',  'DELETE'])
+def note_id(request, id):
+   try:
+       note = Note.objects.get(id=id)
+   except Note.DoesNotExist:
+       return Response(status=status.HTTP_404_NOT_FOUND)
+
+   if request.method == 'GET':
+       serializer = NoteSerializer(note, context={'request':request})
+       return Response(serializer.data)
+
+   
+   elif request.method == 'DELETE':
+       note.delete()
+       return Response(status=status.HTTP_200_OK)
+    
+
+@api_view(['GET', 'POST', 'PUT'])
+def notes(request):
+
+    if request.method == 'GET':
+       notes = Note.objects.all()
+       serializer = NoteSerializer(notes, context={'request': request} ,many=True)
+       return Response(serializer.data)
+
+    elif request.method == 'POST':
+       serializer = NoteSerializer(data=request.data)
+       if serializer.is_valid():
+           serializer.save()
+           return Response(serializer.data)
+       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'PUT':
+        note = Note.objects.get(id=request.data["id"])
+        serializer = NoteSerializer(note, data=request.data, context= {'request':request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)

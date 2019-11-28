914f2ff8cd47260963408f08ee614ad8ed580408
Breno Ferreira <brenoleite1234@gmail.com>
2019-11-28 09:06:25 -0300

    Removing User API View

--- back/app/views.py
+++ back/app/views.py
@@ -46,14 +46,3 @@ def notes(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-
-@api_view(['GET'])
-def user_id(request, id):
-    try:
-        user = User.objects.get(id=id)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'GET':
-       serializer = UserLoginSerializer(user, context={'request':request})
-       return Response(serializer.data)
